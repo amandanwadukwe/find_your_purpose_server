e3c20064b7bbf94e1516588e6a412ad108452067
Amanda Nwadukwe <nwadukwe@lancaster.ac.uk>
2022-09-19 14:14:26 +0100

    fixed bug in login functionality

--- server.js
+++ server.js
@@ -45,13 +45,13 @@ app.get("/", (req,res) => {
     })
 })
 app.get("/:emailAndPassword", (req,res) => {
-    // const emailAndPassword = req.params.emailAndPassword.split("-");
-    pool.query('SELECT * FROM accounts')
+    const emailAndPassword = req.params.emailAndPassword.split("-");
+        console.log(emailAndPassword)
+    pool.query(`SELECT * FROM accounts WHERE email='${emailAndPassword[0]}' AND account_password=crypt('${emailAndPassword[1]}', account_password);`)
     .then(result=>{
-        const emailAndPassword = req.params.emailAndPassword.split("-");
-        // res.json({email:emailAndPassword[0],
-        // password:emailAndPassword[1]})
-        res.json(result.rows.filter(account => account.email === emailAndPassword[0] && account.account_password === emailAndPassword[1]))
+        
+        
+        res.json(result.rows)
     })
     .catch(error=> {
         console.error(error);
@@ -60,8 +60,9 @@ app.get("/:emailAndPassword", (req,res) => {
 })
 
 app.get("/account/:email", (req,res) => {
-    pool.query('SELECT * FROM accounts')
-    .then(result=>res.json(result.rows.filter(account => account.email === req.params.email)))
+    pool.query('SELECT * FROM accounts;')
+    .then(result=>{
+        res.json(result.rows.filter(account => account.email === req.params.email))})
     .catch(error=> {
         console.error(error);
         res.status(500).json(error)
@@ -70,15 +71,27 @@ app.get("/account/:email", (req,res) => {
 
 //Ceate an account
 app.post("/account", (req,res)=> {
-    pool.query(`INSERT INTO accounts(first_name,last_name,email,account_password, enrolled_courses) VALUES ('${req.body.first_name}', '${req.body.last_name}', '${req.body.email}',crypt('${req.body.account_password}', gen_salt('bf')), '{}',{},'','',{});`)
+    pool.query(`INSERT INTO accounts(first_name,last_name,email,account_password, enrolled_courses, notes, pronouns, nickname, settings, classroom_contributions) VALUES ('${req.body.first_name}', '${req.body.last_name}', '${req.body.email}',crypt('${req.body.account_password}', gen_salt('bf')), '{}','{}','','','{}', '{}');`)
+    .then(result=>{
+        res.json(result)
+    })
+    .catch(error=> {
+        console.error(error);
+        res.status(500).json(error)
+    })
 })
 
-// INSERT INTO accounts(first_name,last_name,email,account_password, enrolled_courses, notes, pronouns,nickname, settings, classroom_contributions) VALUES ('Amanda', 'Nwadukwe', 'amandanwadukwe@gmail.com','1234', '{{1,1,2}}', '{}');
 
 //Update last page viewed
 app.post("/last-viewed-page", (req,res)=> {
-    console.log(req.body.activeCourseCode)
     pool.query(`UPDATE accounts SET enrolled_courses='{{${req.body.activeCourseCode},${req.body.activeChapterNumber},${req.body.activePageNumber}}}' WHERE email='${req.body.email}';`)
+    .then(result => console.log("Result: ", result))
+    .catch(err => console.log("Error: ", err))
+})
+
+//Update name
+app.post("/name", (req,res)=>{
+    pool.query(`UPDATE accounts SET first_name='${req.body.new_first_name}', last_name='${req.body.new_last_name}' WHERE email='${req.body.new_first_name}';`)
     .then(result => console.log("Result: ", result))
     .catch(err => console.log("Error: ", err))
 })
